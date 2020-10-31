87dc147e8d9cc0015b793596da964dd194ad113f
hiro <>
2020-10-31 16:17:23 +0900

    commit!

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,5 +6,9 @@
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
 
-Article.create({title: 'ビジョニングの力1', content: '成功者のほぼ全ての人は、ビジョニングを行っている。脳は実際に起きたことと、想像したことを見分けることができない。この力を使ってとにかく自分が成功するイメージを掴むんだ。'})
-Article.create({title: 'ビジョニングの力2', content: '偉人の写真を壁一面に貼ることだ。自分がその人物に近づいているかどうか、日々確認できる。'})
+10.times do
+    Article.create(
+        title: Faker::Lorem.sentence(word_count: 5),
+        content: Faker::Lorem.sentence(word_count: 100)
+    )
+end 
